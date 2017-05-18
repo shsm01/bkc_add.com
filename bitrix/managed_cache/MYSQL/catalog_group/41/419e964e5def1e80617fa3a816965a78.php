271649985b9a3153fbee87b2082f703a0a2b7126
shsm <shsm01@yandex.ru>
2017-05-18 15:09:23 +0400

    New commit #2

--- bitrix/managed_cache/MYSQL/catalog_group/41/419e964e5def1e80617fa3a816965a78.php
+++ bitrix/managed_cache/MYSQL/catalog_group/41/419e964e5def1e80617fa3a816965a78.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001494567251';
-$dateexpire = '001494578051';
+$datecreate = '001495105356';
+$dateexpire = '001495116156';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{i:2;a:6:{s:2:"ID";s:1:"2";s:4:"NAME";s:4:"BASE";s:4:"BASE";s:1:"Y";s:4:"SORT";s:3:"100";s:6:"XML_ID";s:4:"BASE";s:9:"NAME_LANG";s:27:"Розничная цена";}}}';
 return true;
 ?>
